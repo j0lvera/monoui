6314afe1489ab8dfd45a4bb9db1afa4276757342
Juan Olvera <juan@jolvera.dev>
2024-01-16 09:33:14 -0600

    wip: prepare for packaging

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
 export { Box } from "./components/Box";
 export { Button } from "./components/Button";
+export { Card } from "./components/Card";
+export { Divider } from "./components/Divider";
+export { Input } from "./components/Input";
+export { Modal } from "./components/Modal";
+export { Select } from "./components/Select";
 export { cx } from "./utils";
